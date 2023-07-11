553fb12d8a9ccdbe622b7211059d2d65f3193969
Alex Lustig <exoxander15@gmail.com>
2023-07-10 20:03:42 -0600

    new view sets and test data added

--- bookapi/bookapi/urls.py
+++ bookapi/bookapi/urls.py
@@ -2,13 +2,22 @@ from django.contrib import admin
 from django.urls import path
 from django.urls import include
 from rest_framework import routers
-from books.views import BookViewSet, CategoryViewSet
+from books.views import *
 from django.conf.urls.static import static
 from django.conf import settings
 
 router = routers.SimpleRouter()
 router.register("books",BookViewSet)
 router.register("categories", CategoryViewSet)
+router.register("space", SpaceViewSet)
+router.register("science", ScienceViewSet)
+router.register("medieval", MedievalViewSet)
+router.register("engineering", EngineeringViewSet)
+router.register("medicine", MedicineViewSet)
+router.register("programming", ProgrammingViewSet)
+router.register("technology", TechnologyViewSet)
+router.register("biology", BiologyViewSet)
+router.register("art", ArtViewSet)
 
 urlpatterns = [
     path("", include(router.urls)),
--- bookapi/books/views.py
+++ bookapi/books/views.py
@@ -12,3 +12,39 @@ class BookViewSet(viewsets.ModelViewSet):
 class CategoryViewSet(viewsets.ModelViewSet):
     queryset = BookData.objects.filter(category="none")
     serializer_class = BookSerializer
+
+class SpaceViewSet(viewsets.ModelViewSet):
+    queryset = BookData.objects.filter(category="space")
+    serializer_class = BookSerializer
+
+class ScienceViewSet(viewsets.ModelViewSet):
+    queryset = BookData.objects.filter(category="science")
+    serializer_class = BookSerializer
+
+class MedievalViewSet(viewsets.ModelViewSet):
+    queryset = BookData.objects.filter(category="medieval")
+    serializer_class = BookSerializer
+
+class EngineeringViewSet(viewsets.ModelViewSet):
+    queryset = BookData.objects.filter(category="engineering")
+    serializer_class = BookSerializer
+
+class MedicineViewSet(viewsets.ModelViewSet):
+    queryset = BookData.objects.filter(category="medicine")
+    serializer_class = BookSerializer
+
+class ProgrammingViewSet(viewsets.ModelViewSet):
+    queryset = BookData.objects.filter(category="programming")
+    serializer_class = BookSerializer
+
+class TechnologyViewSet(viewsets.ModelViewSet):
+    queryset = BookData.objects.filter(category="technology")
+    serializer_class = BookSerializer
+
+class BiologyViewSet(viewsets.ModelViewSet):
+    queryset = BookData.objects.filter(category="biology")
+    serializer_class = BookSerializer
+
+class ArtViewSet(viewsets.ModelViewSet):
+    queryset = BookData.objects.filter(category="art")
+    serializer_class = BookSerializer
